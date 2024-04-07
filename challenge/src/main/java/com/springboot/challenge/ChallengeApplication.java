cee8aa62b9b138c07f969f7c215fe5d9d46cc1da
douglasmcl <douglasmacielaraujo@outlook.com>
2024-04-07 02:16:55 -0300

    create the Post class

--- challenge/src/main/java/com/springboot/challenge/ChallengeApplication.java
+++ challenge/src/main/java/com/springboot/challenge/ChallengeApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.challenge.challenge;
+package com.springboot.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ challenge/src/main/java/com/springboot/challenge/entities/Post.java
@@ -0,0 +1,78 @@
+package com.springboot.challenge.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String author;
+    private Date date;
+    private String title;
+    private String description;
+    private List<Comment> comments;
+
+    public Post(Long id, String author, Date date, String title, String description, List<Comment> comments) {
+        this.id = id;
+        this.author = author;
+        this.date = date;
+        this.title = title;
+        this.description = description;
+        this.comments = comments;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }    
+}
