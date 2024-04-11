a7e3f7a490b5933d48856a376b34779553a5caea
douglasmcl <douglasmacielaraujo@outlook.com>
2024-04-11 14:25:55 -0300

    create some classes to security

--- challenge/src/main/java/com/springboot/challenge/security/User.java
+++ challenge/src/main/java/com/springboot/challenge/security/User.java
@@ -1,12 +1,14 @@
 package com.springboot.challenge.security;
 
+import java.io.Serializable;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Table(name = "users")
-public class User {
+public class User implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
